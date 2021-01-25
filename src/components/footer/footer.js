799e8b4c58c9e9168084e6b4c2f1f5bde13e5b5f
Dhananjay Singh Kushwaha <d_kushwaha@apple.com>
2021-01-25 20:13:03 +0530

    added footer button styling

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,6 +3,26 @@ import "./footer.scss";
 
 export default class Footer extends Component {
   render() {
-    return <div className="footer">Footer</div>;
+    return (
+      <div className="footer">
+        {/* Links don't work */}
+        <div className="contact">
+          <ul>
+            <li>
+              <div className="social">
+                <img src="../assets/github.png" />
+                Github
+              </div>
+            </li>
+            <li>
+              <div className="social">
+                <img src="../assets/linkedin.png" />
+                LinkedIn
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
